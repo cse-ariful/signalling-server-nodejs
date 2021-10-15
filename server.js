6a73f492e486b01c52e2e4d60b94bfbddb04a895
cse.ariful@gmail.com <cse.ariful@gmail.com>
2021-10-15 18:27:42 +0600

    added client list

--- server.js
+++ server.js
@@ -41,25 +41,47 @@ app.set('views', './');
 const wss = new WebSocketServer({ server });
 
 
-let clients = {};
+let users = {};
 
 wss.on('connection', function (socket) {
+  socket.is_alive = true;
+  socket.on('pong', () => { socket.is_alive = true; });
   socket.on('message', function (message) {
     //console.log(message);
     if (message == undefined) return;
     let payload = JSON.parse(message);
-    let user = payload.user;
-    if (user == undefined) {
-      console.log("undefined user ignoring message");
-      return;
-    }
     console.log("message Recieved  from " + socket.userId + " sending to " + payload.user + " type " + payload.type);
-    if (payload.type.toLowerCase() == "new user") {
-      socket.userId = user;
-      console.log("User registered " + user);
-    } else {
-      wss.broadcast(user, payload);
+    let type = payload.type.toLowerCase();
+    switch (type) {
+      case "new user":
+        if (payload.user === undefined || payload.message.name === undefined) {
+          socket.send(JSON.stringify({ "type": "info", "message": "please set user and message with name for registering user" }));
+          break;
+        }
+        socket.userId = payload.user;
+        let userobj = {};
+        userobj.id = payload.user;
+        userobj.name = payload.message.name;
+        users[payload.user] = userobj;
+        console.log("User registered " + JSON.stringify(users));
+        socket.send(JSON.stringify({ "type": "info", "message": "user subscribed", "user": userobj }));
+        wss.clients.forEach((it => it.send(JSON.stringify({ "type": "user_joined", "message": "A New user joined" }))))
+        break;
+      case "OFFER":
+      case "ANSWER":
+      case "ice":
+        if (payload.user == null) {
+          console.log("target user not set ignoring");
+          return;
+        }
+        wss.broadcast(payload.user, payload);
+        break;
+      case "active_users":
+        console.log("get user list from " + socket.userId);
+        socket.send(JSON.stringify(users));
+        break;
     }
+
 
   });
   socket.on('error', function (e) { });
@@ -87,5 +109,12 @@ wss.broadcast = function (user, data) {
   }
 
 };
+// setInterval(function ping() {
+//   Array.from(wss.clients).forEach(function each(client_stream) {
+//     if (!client_stream.is_alive) { client_stream.terminate(); return; }
+//     client_stream.is_alive = false;
+//     client_stream.send(JSON.stringify({ "type": "ping", "message": "Reply with pong if you are active" }));
+//   });
+// }, 5000);
 
 console.log('Server running. Visit https://' + getLocalIpAddress() + ":" + HTTP_PORT + '.\n\n');
